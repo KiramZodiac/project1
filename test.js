7e3496f13eae23e7caae744f7c69d172309a67c6
akrammageye <kiram751214095@gmail.com>
2023-08-01 15:19:14 +0300

    my to do app

---
+++ test.js
@@ -0,0 +1,74 @@
+//1. Interactive To-Do List
+
+//Create a simple to-do list application that allows users to add,
+ //remove, and mark tasks as complete. Firstly, start with a basic HTML structure.
+  //Then, use JavaScript to handle user interactions and update the list dynamically. Next, focus on implementing features like adding new tasks, checking off completed tasks, and deleting tasks. Additionally, enhance the project by incorporating features such as storing tasks in the
+ //browser's local storage, enabling task prioritization, or adding due dates.
+
+
+const input = document.querySelector('#input')
+const btn = document.querySelector('#btn');
+
+const todos = document.querySelector('#todos');
+const save = document.querySelector('#save');
+
+
+btn.addEventListener('click',function(){
+let li = document.createElement('li')
+
+li.innerHTML = input.value
+todos.appendChild(li)
+
+
+
+li.addEventListener('click',function(){
+
+    li.remove()
+})
+})
+
+save.addEventListener('click',function(){
+
+    let p = document.createElement('p');
+    p.innerHTML = input.value;
+
+
+ })
+
+
+ const bt = document.querySelector('bt')
+
+ 
+
+
+
+
+ setTimeout(() => {
+    document.body.style.backgroundColor = 'red' 
+
+ },1000);
+
+
+
+ setTimeout(() => {
+    document.body.style.backgroundColor ='pink'
+},1000 );
+
+const sing = async ()=>{
+return 'alalalal'
+
+}
+sing().then((data)=>{
+console.log('function,',data)
+
+})
+
+
+
+
+
+
+
+
+
+
